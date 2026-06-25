fca4d819c54aa2f9699308cef7ff406e4adb720c
SHAHIDDIN SHAIK <mrshyam0923@gmail.com>
2026-06-25 11:16:08 +0530

    Add files via upload

---
+++ range_task.py
@@ -0,0 +1,6 @@
+# check number 1 to 100 in range prblm
+num = int(input("Enter a number:"))
+if num in range(1,100):
+    print(f"Number {num} in range 1 to 100 !")
+else:
+    print(f"Number {num} out of the range...")
